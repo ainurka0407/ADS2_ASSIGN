dab9b9878e306ded28144bd4ee2093e4dea59e2a
ainurka0407 <a.nurzhanova111@gmail.com>
2024-04-10 08:43:40 +0500

    I have added the description part of the first task, also, solved the problem2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+/**
+ * This method find the minimum number from the given array.
+ * It uses recursion.
+ * Time complexity is O(n), where n is the input number.
+ * The base case of it when n is 1, so the minimum number will be this number with zero index.
+ In the recursive case, it recursively finds the minimum element in the rest of the array (excluding the last element) and stores it in minInRest.
+ Then, it compares minInRest with the last element of the array (arr[n - 1]) and returns the smaller of the two using Math.min.
+ * This will continue until it reaches the base case.
+ */
 public class Main {
     public static int findMin(int[] arr, int n){
         if(n==1){
---
+++ src/prob2.java
@@ -0,0 +1,24 @@
+/**
+ * This method find the average number from the given array.
+ * It uses recursion.
+ * Time complexity is O(n), where n is the input number.
+ * The base case of it when n is 0, so the average number will be 0.
+ * In recursive case it adds the value of the element at index n-1 to the sum of elements up to n-1.
+ * This will continue until it reaches the base case.
+ */
+
+
+public class prob2 {
+    public static double FindAverage(int[] arr, int n){
+        if(n==0){
+            return 0;
+        }
+        return arr[n-1]+ FindAverage(arr,n-1);
+    }
+    public static void main(String[] args){
+        int n=4;
+        int [] arr = {3,2,4,1};
+        System.out.println(FindAverage(arr,n)/n);
+
+    }
+}
